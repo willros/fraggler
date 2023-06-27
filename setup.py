76e0ac39e3c688b69ab8014309470cdfed6436c7
Williams UMU-MacBook <william.rosenbaum@umu.se>
2023-06-27 12:43:06 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 import setuptools
-from setuptools import setup
+from pathlib import Path
 
-setup(
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setuptools.setup(
     name="fraggler",
-    version="0.1.0",
+    version="0.1.1",
     description="Fragment Analysis package in python!",
     url="https://github.com/willros/fraggler",
     author="William Rosenbaum and Pär Larsson",
     author_email="william.rosenbaum@umu.se",
     license="MIT",
     packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown'
     python_requires=">=3.10",
     install_requires=[
         "pandas",
