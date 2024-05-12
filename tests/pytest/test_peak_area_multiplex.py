828c8e077337b8bb010165a7cb59dd5467de112f
Williams UMU-MacBook <william.rosenbaum@umu.se>
2024-05-12 22:15:15 +0200

    workflows

--- tests/pytest/test_peak_area_multiplex.py
+++ tests/pytest/test_peak_area_multiplex.py
@@ -22,8 +22,8 @@ def test_peak_model(pam):
     gauss = pam.fit_assay_peaks("gauss", 1, "test")
     voigt = pam.fit_assay_peaks("voigt", 1, "test")
 
-    assert round(voigt.fitted_peak_height.to_list()[0], 2) == 731.83
-    assert round(gauss.fitted_peak_height.to_list()[0], 2) == 708.33
+    assert round(voigt.fitted_peak_height.to_list()[0], 1) == 731.8
+    assert round(gauss.fitted_peak_height.to_list()[0], 1) == 708.3
 
     assert gauss.file_name.to_list()[0] == "multiplex.fsa"
     assert voigt.file_name.to_list()[0] == "multiplex.fsa"
@@ -31,5 +31,5 @@ def test_peak_model(pam):
     assert gauss.assay_name.to_list()[0] == "test"
     assert voigt.assay_name.to_list()[0] == "test"
 
-    assert round(gauss.area.to_list()[0], 2) == 3117.44
-    assert round(voigt.area.to_list()[0], 2) == 3705.64
+    assert round(gauss.area.to_list()[0], 1) == 3117.4
+    assert round(voigt.area.to_list()[0], 1) == 3705.6
